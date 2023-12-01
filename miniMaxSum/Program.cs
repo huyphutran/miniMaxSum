f4577313f0e820a61a3acce3e2a15b1c4b5c891a
Phu Tran <tranhuyphuhp75@gmail.com>
2023-12-01 13:48:09 +0700

    Init method miniMaxSum to git

---
+++ miniMaxSum/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("Please enter numbers !");
+List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+Console.Clear();
+
+
+Result.miniMaxSum(arr);
---
+++ miniMaxSum/Result.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Result
+{
+    public static void miniMaxSum(List<int> arr) {
+
+        int sum = 0;
+        int minNum = 0;
+        int maxNum = 0;
+        List<int> evenlist = new List<int>();
+        List<int> oddList = new List<int>();
+        for (int i = 0; i < arr.Count; i++)
+        {
+               sum += arr[i];
+            if (arr[i] < minNum)
+                minNum = arr[i];
+            if (arr[i] > maxNum)
+                maxNum = arr[i];
+            if (arr[i] % 2 == 0)
+                evenlist.Add(arr[i]);
+            if (arr[i] % 2 != 0)
+                oddList.Add(arr[i]);
+
+        }
+        int minSum = sum - minNum;
+        int maxSum = sum - maxNum;
+
+        Console.WriteLine(minSum + " " + maxSum);
+        Console.WriteLine("Even numbers in List " + string.Join(",",evenlist));
+        Console.WriteLine("Odd numbers in List " + string.Join(",", oddList));
+
+    }
+}
